5065b2277f19e4c59300987cf297aa0b5b74b2b0
asm0dey <pavel.finkelshtein@gmail.com>
2021-02-27 16:18:42 +0300

    Update with movielens example

--- src/main/scala/gradle/spark/quickstart/App.scala
+++ src/main/scala/gradle/spark/quickstart/App.scala
@@ -10,16 +10,51 @@ object App {
     some().show()
   }
 
-  def some(): Dataset[Int] = {
+  def some(): Dataset[MovieWithSingleGenre] = {
+    val spark: SparkSession = sparkFactory
+    import spark.implicits._
+    val source = spark.read
+      .option("header", true)
+      .option("inferSchema", true)
+      .csv("/home/finkel/Downloads/ml-latest/movies.csv")
+      .as[Movie]
+    pipeline(source)(spark)
+
+  }
+
+  def pipeline(source: Dataset[Movie])(implicit spark: SparkSession) = {
+    import spark.implicits._
+    source
+      .filter(_.title.matches("""^"?.*\(\d{4}\)\s*"?$"""))
+      .map(m => {
+        val lastOpen = m.title.lastIndexOf("(")
+        val year = m.title.substring(lastOpen + 1, lastOpen + 5).toInt
+        MovieWithYear(m.movieId, m.title.replaceAll("""\(\d{4}\)\s*"?$""", "").trim, m.genres, year)
+      })
+      .map(m => MovieWithGenresAndYear(m.movieId, m.title, m.genres.split("\\|"), m.year))
+      .flatMap(movie => movie.genres.map(genre => MovieWithSingleGenre(movie.movieId, movie.title, genre, movie.year)))
+      .filter(_.genre != "(no genres listed)")
+  }
+
+  private def sparkFactory = {
     val spark = SparkSession
       .builder()
       .appName("test")
       .master("local")
       .getOrCreate()
-    import spark.implicits._
     spark
-      .sql("SELECT 1")
-      .toDF()
-      .as[Int]
   }
 }
+
+case class Movie(movieId: Long, title: String, genres: String)
+
+case class MovieWithYear(movieId: Long, title: String, genres: String, year: Integer)
+
+case class MovieWithGenresAndYear(movieId: Long, title: String, genres: Array[String], year: Integer)
+
+case class MovieWithSingleGenre(movieId: Long, title: String, genre: String, year: Integer)
+
+case class MovieExploded(movieId: Long, title: String, genres: Array[String])
+
+case class MovieAggregate(year: Int, count: Long)
+
--- src/test/scala/gradle/spark/quickstart/AppSuite.scala
+++ src/test/scala/gradle/spark/quickstart/AppSuite.scala
@@ -3,14 +3,28 @@
  */
 package gradle.spark.quickstart
 
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.scalatest.funsuite.AnyFunSuite
 import org.junit.runner.RunWith
 import org.scalatestplus.junit.JUnitRunner
 
+import java.util
+
 @RunWith(classOf[JUnitRunner])
 class AppSuite extends AnyFunSuite {
+  val spark = SparkSession.builder().master("local").getOrCreate()
   test("App has a greeting") {
-    assert(App.some().collectAsList().get(0) == 1)
+    import spark.implicits._
+    val movieWithGenres = App.pipeline(
+      Seq(
+        Movie(1L, "MyMovie (1994)", "Drama|Comedy|Thriller")
+      ).toDS()
+    )(spark)
+      .collectAsList()
+    assert(movieWithGenres.containsAll(util.Arrays.asList(
+      MovieWithSingleGenre(1L, "MyMovie", "Drama", 1994),
+      MovieWithSingleGenre(1L, "MyMovie", "Comedy", 1994),
+      MovieWithSingleGenre(1L, "MyMovie", "Thri ller", 1994)
+    )))
   }
 }
